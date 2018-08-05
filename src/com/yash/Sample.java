63335a4e136543b2127f1224e895583a48da52ea
Syed <sfdev@gmail.com>
2018-08-04 23:32:49 -0500

    commit 2

--- src/com/yash/Sample.java
+++ src/com/yash/Sample.java
@@ -2,7 +2,7 @@ package com.yash;
 
 public class Sample {
     public static void main(String[] args) {
-        for(int i=1; i<=10; i++){
+        for(int i=1; i<=5; i++){
             System.out.println("Hi ... "+i);
         }
     }

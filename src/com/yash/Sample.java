a6b977f3ded3c6862e6b10314f4566a8d08f0114
Syed <sfdev@gmail.com>
2018-08-04 23:25:47 -0500

    Initial Commit

---
+++ src/com/yash/Sample.java
@@ -0,0 +1,9 @@
+package com.yash;
+
+public class Sample {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++){
+            System.out.println("Hi ... "+i);
+        }
+    }
+}
